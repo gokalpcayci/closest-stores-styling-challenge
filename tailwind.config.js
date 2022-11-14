fcade27265d06cbe147e9222501bd5b9de2d41e0
gokalpcayci <92229750+gokalpcayci@users.noreply.github.com>
2022-11-14 17:14:44 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        veryDarkBlue: "#121129",
+        skin: "#FFFFFF",
+        bluish: "#2A2753",
+        darkBlue: "#1C1A39",
+        darkBlue2: "#121129",
+        pink: "#FF5EAB",
+        purple: "#756BFF",
+        turquoise: "#00BD9B",
+        grayish: "#C4C0FF",
+      },
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
